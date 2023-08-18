de57d4ac71cd4849f733be58b48fec9b04e12594
davidddx <136852347+davidddx@users.noreply.github.com>
2023-08-18 14:53:20 -0400

    added internal life system, collision w/ enemy or going oob causes plrlives -= 1

---
+++ pygameproj/Tilesets/objectTileSet.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="objectTileSet" tilewidth="512" tileheight="256" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="Enemy">
+  <properties>
+   <property name="name" value="BlueEnemy"/>
+  </properties>
+  <image width="128" height="192" source="../tiles/EnemySprite.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="plr"/>
+  </properties>
+  <image width="128" height="192" source="../tiles/plrsprite.png"/>
+ </tile>
+ <tile id="2" type="Door">
+  <properties>
+   <property name="name" value="door"/>
+  </properties>
+  <image width="192" height="256" source="../tiles/door.png"/>
+ </tile>
+ <tile id="3" type="OutOfBounds">
+  <properties>
+   <property name="name" value="OutOfBounds"/>
+  </properties>
+  <image width="512" height="128" source="../tiles/OutOfBounds.png"/>
+ </tile>
+ <tile id="4" type="PlrSpawnPoint">
+  <properties>
+   <property name="name" value="plrspawn"/>
+  </properties>
+  <image width="256" height="256" source="../tiles/PlrSpawnPoint.png"/>
+ </tile>
+ <tile id="5" type="BlueEnemySpawner">
+  <properties>
+   <property name="name" value="BlueEnemySpawner"/>
+  </properties>
+  <image width="128" height="128" source="../tiles/BlueEnemySpawner.png"/>
+ </tile>
+ <tile id="6" type="EnemySpawner">
+  <properties>
+   <property name="name" value="RedEnemySpawner"/>
+  </properties>
+  <image width="128" height="128" source="../tiles/RedEnemySpawner.png"/>
+ </tile>
+</tileset>
