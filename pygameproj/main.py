beef768ee4a9f15e73f493dadc59fa79d54bdcbb
davidddx <136852347+davidddx@users.noreply.github.com>
2023-08-30 12:35:02 -0400

    shurikens despawn on wall and player contact

--- pygameproj/main.py
+++ pygameproj/main.py
@@ -16,8 +16,8 @@ globals.levelhandler = levelhandler()
 
 running = true;
 while running:
-    clock.tick(30) # makes max fps 45
-    # print("fps: ", clock.get_fps())
+    clock.tick(30) # makes max fps 30
+    print("fps: ", clock.get_fps())
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit();
