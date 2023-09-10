247efae5b7e90058c05f575935da1935d2576971
davidddx <david0319072@gmail.com>
2023-09-09 22:28:12 -0400

    level editing

--- pygameproj/Tilesets/backgroundimages.tsx
+++ pygameproj/Tilesets/backgroundimages.tsx
@@ -1,24 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="backgroundimages" tilewidth="1480" tileheight="576" tilecount="17" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="backgroundimages" tilewidth="450" tileheight="450" tilecount="11" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="3">
-  <properties>
-   <property name="name" value="background"/>
-  </properties>
-  <image width="480" height="480" source="../tiles/background/tree.png"/>
- </tile>
- <tile id="4">
-  <properties>
-   <property name="name" value="background"/>
-  </properties>
-  <image width="1024" height="576" source="../tiles/background/hillybackground.png"/>
- </tile>
- <tile id="5">
-  <properties>
-   <property name="name" value="background"/>
-  </properties>
-  <image width="1480" height="576" source="../tiles/background/bighillbackground.png"/>
- </tile>
  <tile id="6">
   <properties>
    <property name="name" value="background"/>
@@ -84,23 +66,5 @@
    <property name="name" value="background"/>
   </properties>
   <image width="450" height="450" source="../tiles/background/cloud3.png"/>
- </tile>
- <tile id="17">
-  <properties>
-   <property name="name" value="background"/>
-  </properties>
-  <image width="576" height="576" source="../tiles/background/leaflesstree.png"/>
- </tile>
- <tile id="18">
-  <properties>
-   <property name="name" value="background"/>
-  </properties>
-  <image width="300" height="300" source="../tiles/background/leaflesstreesmaller.png"/>
- </tile>
- <tile id="19">
-  <properties>
-   <property name="name" value="background"/>
-  </properties>
-  <image width="171" height="171" source="../tiles/background/leaflesstreesmallest.png"/>
  </tile>
 </tileset>
---
+++ pygameproj/Tilesets/savannahgrasslands.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="savannahgrasslands" tilewidth="576" tileheight="576" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="background"/>
+  </properties>
+  <image width="576" height="576" source="../tiles/background/leaflesstree.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="background"/>
+  </properties>
+  <image width="576" height="576" source="../tiles/background/leaflesstree2.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="background"/>
+  </properties>
+  <image width="300" height="300" source="../tiles/background/leaflesstreesmaller.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="background"/>
+  </properties>
+  <image width="256" height="256" source="../tiles/background/tallgrass.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="background"/>
+  </properties>
+  <image width="256" height="256" source="../tiles/background/tallgrassleftedge.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="background"/>
+  </properties>
+  <image width="256" height="256" source="../tiles/background/tallgrassrightedge.png"/>
+ </tile>
+</tileset>
